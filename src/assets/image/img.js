a6b5959518c0251c6c77dc18eaac86fda65d5861
MD AFJAL <ASARAF>
2026-01-12 11:56:31 +0530

    fix1

--- src/assets/image/img.js
+++ src/assets/image/img.js
@@ -14,61 +14,48 @@ import pattern from "../image/pattern.png";
 import Client_1 from "../image/client1.png";
 import Client_2 from "../image/client2.png";
 import Client_3 from "../image/client3.png";
-import Hosting from "../image/Hosting.png";
+import Hosting from "../image/hosting.png";
 import Layers_2 from "../image/Layers_2.png";
 import LogoNew from "../image/logo_new.png";
 import LogoNew2 from "../image/logo_new2.png";
-import circles_2 from '../image/circles_2.png'
-import Frames_2 from '../image/Frames_2.png'
-import owner from '../image/owner.jpeg'
-import objectss from '../image/objectss.png'
-import One from '../image/One.png'
-import Two from '../image/Two.png'
-import Three from '../image/Three.png'
-import Four from '../image/Four.png'
-import Five from '../image/Five.png'
-import Six from '../image/Six.png'
-import Seven from '../image/Seven.png'
-import Eight from '../image/Eight.png'
-import Nine from '../image/Nine.png'
-import Ten from '../image/Ten.png'
-import Eleven from '../image/Eleven.png'
-import Twelve from '../image/Twelve.png'
-import Thirteen from '../image/Thirteen.png'
-import Fourteen from '../image/Fourteen.png'
-import Fifteen from '../image/Fifteen.png'
-import Sixteen from '../image/Sixteen.png'
-import Seventeen from '../image/Seventeen.png'
-import Eighteen from '../image/Eighteeen.png'
-import Ninteen from '../image/Ninteenss.png'
-import Twenty from '../image/Twenty.png'
-import Profile_1 from '../image/profile_1.png'
-import Profile_2 from '../image/profile_2.png'
-import Profile_3 from '../image/profile_3.png'
+import circles_2 from "../image/circles_2.png";
+import Frames_2 from "../image/Frames_2.png";
+import owner from "../image/owner.jpeg";
+import objectss from "../image/objectss.png";
+import One from "../image/One.png";
+import Two from "../image/Two.png";
+import Three from "../image/Three.png";
+import Four from "../image/Four.png";
+import Five from "../image/Five.png";
+import Six from "../image/Six.png";
+import Seven from "../image/Seven.png";
+import Eight from "../image/Eight.png";
+import Nine from "../image/Nine.png";
+import Ten from "../image/Ten.png";
+import Eleven from "../image/Eleven.png";
+import Twelve from "../image/Twelve.png";
+import Thirteen from "../image/Thirteen.png";
+import Fourteen from "../image/Fourteen.png";
+import Fifteen from "../image/Fifteen.png";
+import Sixteen from "../image/Sixteen.png";
+import Seventeen from "../image/Seventeen.png";
+import Eighteen from "../image/Eighteeen.png";
+import Ninteen from "../image/Ninteenss.png";
+import Twenty from "../image/Twenty.png";
+import Profile_1 from "../image/profile_1.png";
+import Profile_2 from "../image/profile_2.png";
+import Profile_3 from "../image/profile_3.png";
 
-import Profiles_1 from '../image/Profiles_1.png'
-import Profiles_2 from '../image/Profiles_2.png'
-import Profiles_3 from '../image/Profiles_3.png'
-import Profile_4 from '../image/Profile_4.png'
-import Profile_5 from '../image/Profile_5.png'
-import Profile_6 from '../image/Profile_6.png'
-import OnboardingCall from '../image/OnboardingCall.gif'
-import Plannings from '../image/Plannings.gif'
-import Execution from '../image/Execution.gif'
-import Optimizations from '../image/Optimizations.gif'
-
-
-
-
-
-
-
-
-
-
-
-
-
+import Profiles_1 from "../image/Profiles_1.png";
+import Profiles_2 from "../image/Profiles_2.png";
+import Profiles_3 from "../image/Profiles_3.png";
+import Profile_4 from "../image/Profile_4.png";
+import Profile_5 from "../image/Profile_5.png";
+import Profile_6 from "../image/Profile_6.png";
+import OnboardingCall from "../image/OnboardingCall.gif";
+import Plannings from "../image/Plannings.gif";
+import Execution from "../image/Execution.gif";
+import Optimizations from "../image/Optimizations.gif";
 
 export const img = {
   Ellipse,
@@ -121,12 +108,11 @@ export const img = {
   Profiles_2,
   Profiles_1,
   Profiles_3,
-  Profile_4,  
+  Profile_4,
   Profile_5,
   Profile_6,
   OnboardingCall,
   Plannings,
   Execution,
   Optimizations,
-
 };
